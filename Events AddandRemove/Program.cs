e7283465b9883ff6ba663f5d2100d1a1c96de8cb
Kulpot <kulpotwahu@gmail.com>
2023-06-19 20:35:32 +0800

    Add project files.

---
+++ Events AddandRemove/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+//ref link:https://www.youtube.com/watch?v=qAQRzWtfqNQ&list=PLAE7FECFFFCBE1A54&index=18
+//
+
+class Cow
+{
+    //public event Action Mooing;
+    private Action mooing;
+    public event Action Mooing{
+        add 
+        { 
+            mooing += value;
+            mooing += value;
+            mooing += value;
+            Console.WriteLine("addding"); 
+        }
+        remove { mooing -= value; Console.WriteLine("removing"); }
+    }
+    public void PushSleepingCow()
+    {
+        // .....
+        if(mooing != null) 
+            mooing();
+    }
+}
+
+class MainClass
+{
+    static void Main()
+    {
+        Cow c = new Cow();
+        c.Mooing += () => Console.WriteLine("Giggle");
+        c.PushSleepingCow();
+    }
+}
